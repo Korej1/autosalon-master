d9fc18ff97e8ecba6676a791d8c6c3e8c9c94e56
Юрий <cacke.nagato@icloud.com>
2021-12-25 18:23:17 +0300

    Initial commit

---
+++ src/main/java/com/Youra/autosalon/AutosalonApplication.java
@@ -0,0 +1,15 @@
+package com.Youra.autosalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosalonApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AutosalonApplication.class, args);
+		System.out.println("gvchc");
+	}
+
+}
---
+++ src/test/java/com/Youra/autosalon/AutosalonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Youra.autosalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
